240e3b7d59ec6e40323cf375f1594cafcfbb406e
bridgetmabli <>
2022-02-04 21:56:39 +0000

    units of length and name

---
+++ math.py
@@ -0,0 +1,11 @@
+# feet to inches
+def feet_to_inches():
+  how_much_feet = int(input ("how_much_feet"))
+  print(how_much_feet * 12)
+
+# inches to feet
+def inches_to_feet():
+  how_many_inches = int (input ("how_many_inches"))
+  print (int(how_many_inches / 12))
+
+()
---
+++ name.py
@@ -0,0 +1,9 @@
+# print('hi my name is bridget')
+# name = 'sean' 
+# print("hi my name is " + name)
+#name = input('what is your name?')
+
+name = input ("what is your name?")
+how_old_are_you = input ("how old are you?")
+how_are_you = input ("how are you?")
+print (name + ' ' + how_old_are_you + ' ' + how_are_you)
